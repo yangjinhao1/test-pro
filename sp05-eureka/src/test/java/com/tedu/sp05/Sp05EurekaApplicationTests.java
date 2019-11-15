dcabdac346786899d551180bfa7f72c1a761066c
yangjinhao1 <184946099@qq.com>
2019-11-15 23:00:54 +0800

    a

--- sp05-eureka/src/test/java/com/tedu/sp05/Sp05EurekaApplicationTests.java
+++ sp05-eureka/src/test/java/com/tedu/sp05/Sp05EurekaApplicationTests.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class Sp05EurekaApplicationTests {
 
 	@Test
-	public void contextLoads(String name) {
+	public void contextLoads(String name1) {
 	}
 
 }
